a9e13850da03e8d52a40c6c94c50699bfe393be9
Park Min Ki <ur_developer92@naver.com>
2023-08-11 23:14:07 +0900

    PC  chapter06 +example

--- Ch.06.java
+++ Ch.06.java
@@ -39,3 +39,21 @@ public class TvTest2 {
 		System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
 	}
 }
+
+public class TvTest3 {
+
+	public static void main(String[] args) {
+		Tv[] tvArr = new Tv[3]; // 길이가 3인 Tv객체배열
+
+		// Tv객체를 생성해서 Tv객체 배열의 각 요소에 저장
+		for (int i = 0; i < tvArr.length; i++) {
+			tvArr[i] = new Tv();
+			tvArr[i].channel = i + 10; // tvArr[i]의 Channel에 i+10을 저장
+		}
+
+		for (int i = 0; i < tvArr.length; i++) {
+			tvArr[i].channelUp(); // tvArr[i]의 메서드를 호출. 채널이 1증가
+			System.out.printf("tvArr[%d].channel=%d%n", i, tvArr[i].channel);
+		}
+	}
+}
