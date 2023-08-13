eb2eb200e1a25bf7c256a030b0a6ee35f3141217
Park Min Ki <ur_developer92@naver.com>
2023-08-13 19:40:11 +0900

    PC  chapter06 +example add

--- Ch.06.java
+++ Ch.06.java
@@ -57,3 +57,123 @@ public class TvTest3 {
 		}
 	}
 }
+
+public class Ex6_3 {
+	public static void main(String[] args) {
+		System.out.println("Card.width = " + Card.width);
+		System.out.println("Card.height = " + Card.height);
+		
+		Card c1 = new Card();
+		c1.kind = "heart";
+		c1.number = 7;
+		
+		Card c2	= new Card();
+		c2.kind = "Spade";
+		c2.number = 4;
+		
+		System.out.println("c1은 " + c1.kind + ", " + c1.number +  "이며, 크기는 (" + c1.width + ", " + c1.height + ")" );
+		System.out.println("c2은 " + c2.kind + ", " + c2.number +  "이며, 크기는 (" + c2.width + ", " + c2.height + ")" );
+		System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
+		
+		c1.width = 50;
+		c1.height = 80;
+		
+		System.out.println("c1은 " + c1.kind + ", " + c1.number +  "이며, 크기는 (" + c1.width + ", " + c1.height + ")" );
+		System.out.println("c2은 " + c2.kind + ", " + c2.number +  "이며, 크기는 (" + c2.width + ", " + c2.height + ")" );
+	}
+}
+
+public class MyMath {
+	long add(long a, long b) {
+		long result = a + b;
+		return result;
+	// return a + b; // 위의 두 줄을 이와 같이 한 줄로 간단히 할 수 있다.
+	}
+	long subtract(long a, long b) { return a - b; }
+	long multiply(long a, long b) { return a * b; }
+	double divide(double a, double b) {
+		return a / b ;
+	}
+}
+
+
+public class Ex6_4 {
+
+	public static void main(String[] args) {
+		MyMath mm = new MyMath();
+		long result1 = mm.add(5L, 3L);
+		long result2 = mm.subtract(5L, 3L);
+		long result3 = mm.multiply(5L, 3L);
+		double result4 = mm.divide(5L, 3L);
+		
+		System.out.println("add(5L, 3L) = " + result1);
+		System.out.println("subtract(5L, 3L) = " + result2);
+		System.out.println("multiply(5L, 3L) = " + result3);
+		System.out.println("divide(5L, 3L) = " + result4);
+	}
+}
+
+
+public class Data { int x;}
+
+
+public class Ex6_6 {
+
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+		
+		change(d.x);
+		System.out.println("After change(d.x)");
+		System.out.println("main() : x = " + d.x);
+	}
+
+	static void change(int x) {
+		x = 1000;
+		System.out.println("change() : x = " +x);
+	}
+}
+
+public class Data2 { int x;}
+
+public class Ex6_7 {
+
+	public static void main(String[] args) {
+		Data2 d = new Data2();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+		
+		change(d);
+		System.out.println("After change(d)");
+		System.out.println("main() : x = " + d.x);
+	}
+
+		static void change(Data2 d) {
+			d.x = 1000;
+			System.out.println("change() : x = " + d.x);
+		}
+}
+	
+
+public class Data3 { int x;}
+
+public class Ex6_8 {
+
+	public static void main(String[] args) {
+		Data3 d = new Data3();
+		d.x = 10;
+		
+		Data3 d2 = copy(d);
+		System.out.println("d.x ="+d.x);
+		System.out.println("d2.="+d2.x);
+	}
+		
+	static Data3 copy(Data3 d) {
+		Data3 tmp = new Data3();
+		
+		tmp.x = d.x;
+		
+		return tmp;
+	}
+}
