07757473a322b9b4746304f5f556fa9e6835a906
Park Min Ki <ur_developer92@naver.com>
2023-08-16 22:11:19 +0900

    PC chapter07 +add

--- Ch.07.java
+++ Ch.07.java
@@ -46,3 +46,95 @@ public class Ex7_2 {
 		c.method();
 	}
 }
+public class FireEngine extends Car {
+	void water() {
+		System.out.println("watter!!!");
+	}
+}
+
+public class Car {
+	String color;
+	int door;
+
+	void drive() {
+		System.out.println("drive, Brrrr~");
+	}
+
+	void stop() {
+		System.out.println("stop");
+	}
+}
+
+public class Ex7_7 {
+
+	public static void main(String[] args) {
+		Car car = null;
+		FireEngine fe = new FireEngine();
+		FireEngine fe2 = null;
+
+		fe.water();
+		car = fe; // car = (car)fe;에서 형변환이 생략됨
+//		car.water(); // car타입의 참조변수로는 water()를 호출할 수 없다.
+		fe2 = (FireEngine) car;
+		fe2.water();
+	}
+}
+
+public class Product {
+	int price;
+	int bonusPoint;
+
+	Product(int price) {
+		this.price = price;
+		bonusPoint = (int) (price / 10.0);
+	}
+}
+
+public class Tv1 extends Product {
+	Tv1() {
+		super(100);
+	}
+
+	public String toString() {
+		return "Tv";
+	}
+}
+
+public class Computer extends Product {
+	Computer() {
+		super(200);
+	}
+
+	public String toString() {
+		return "computer";
+	}
+}
+
+public class Buyer {
+	int money = 1000;
+	int bonusPoint = 0;
+
+	void buy(Product p) {
+		if (money < p.price) {
+			System.out.println("잔액이 부족하여 물건을 살 수 없습니다.");
+			return;
+		}
+
+		money -= p.price;
+		bonusPoint += p.bonusPoint;
+		System.out.println(p + "을/를 구입하셨습니다.");
+	}
+}
+
+public class Ex7_8 {
+
+	public static void main(String[] args) {
+		Buyer b = new Buyer();
+
+		b.buy(new Tv1());
+		b.buy(new Computer());
+
+		System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
+		System.out.println("현재 보너스점수는 " + b.bonusPoint + "점입니다.");
+	}
+}
