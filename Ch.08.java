90000e145fdad7576ef81b24022971882863330e
Park Min Ki <ur_developer92@naver.com>
2023-08-18 22:26:56 +0900

    PC chapter08 +add2

--- Ch.08.java
+++ Ch.08.java
@@ -43,3 +43,18 @@ public class Ex8_5 {
 		System.out.println(6);
 	} // main 메서드의 끝
 }
+
+public class Ex8_9 {
+
+	public static void main(String[] args) throws Exception {
+		method1(); // 같은 클래스내의 static멤버이므로 객체생성없이 직접 호출가능.
+	} // main메서드의 끝
+
+	static void method1() throws Exception {
+		method2();
+	} // method1의 끝
+
+	static void method2() throws Exception {
+		throw new Exception();
+	} // method2의 끝
+}
