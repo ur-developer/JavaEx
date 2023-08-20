4360a61d69e35f352395190d7a9fdbcca5d1d49d
Park Min Ki <ur_developer92@naver.com>
2023-08-20 22:27:26 +0900

    PC chapter09 add2

--- Ch.09.java
+++ Ch.09.java
@@ -61,3 +61,28 @@ public class Ex9_3 {
 		System.out.println(System.identityHashCode(str2));
 	}
 }
+
+// Ex9_4
+public class Ex9_4 {
+	public static void main(String[] args) {
+		Card c1 = new Card();
+		Card c2 = new Card();
+
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+	}
+}
+
+public class Card {
+	String kind;
+	int number;
+
+	Card() {
+		this("SPADE", 1);
+	}
+
+	Card(String kind, int number) {
+		this.kind = kind;
+		this.number = number;
+	}
+}
