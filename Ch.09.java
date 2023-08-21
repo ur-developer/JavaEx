f60ba15d06245914e0f0204b4463758a447880ea
Park Min Ki <ur_developer92@naver.com>
2023-08-21 21:36:14 +0900

    PC chapter09 add3

--- Ch.09.java
+++ Ch.09.java
@@ -86,3 +86,32 @@ public class Card {
 		this.number = number;
 	}
 }
+
+public class Ex9_5 {
+
+	public static void main(String[] args) {
+		Card2 c1 = new Card2();
+		Card2 c2 = new Card2("HEART", 10);
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+	}
+}
+
+public class Card2 {
+	String kind;
+	int number;
+
+	Card2() {
+		this("SPADE", 1); // Card(String kind, int number)를 호출
+	}
+
+	Card2(String kind, int number) {
+		this.kind = kind;
+		this.number = number;
+	}
+
+	public String toString() {
+		// Card2인스턴스의 kind와 number를 문자열로 반환한다.
+		return "kind : " + kind + ", number : " + number;
+	}
+}
