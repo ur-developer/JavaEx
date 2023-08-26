c4ea3110149fafa75d75053fc271f7653a03a9fc
Park Min Ki <ur_developer92@naver.com>
2023-08-26 23:28:32 +0900

    PC chapter10 add2

--- Ch.10.java
+++ Ch.10.java
@@ -20,5 +20,26 @@ public class Ex_7 {
 		}
 	}
 }
-<console>
+<Console>
 1,234,567.89 -> 1234567.89 -> 123.5E4
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+//Ex10_9
+public class Ex10_9 {
+	public static void main(String[] args) {
+		DateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
+		DateFormat df2 = new SimpleDateFormat("yyyy/MM/dd");
+
+		try {
+			Date d = df.parse("2019년 11월 23일");
+			System.out.println(df2.format(d));
+		} catch (Exception e) {
+		}
+	}
+}
+<Console>
+2019/11/23
