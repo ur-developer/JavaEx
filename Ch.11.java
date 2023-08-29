a538e5a7f257b593a0532a8e16ef314addeaf03e
Park Min Ki <ur_developer92@naver.com>
2023-08-29 22:50:36 +0900

    PC chapter11 add2

--- Ch.11.java
+++ Ch.11.java
@@ -1,8 +1,5 @@
 //Ex11_1
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Ex11_1 {
 	public static void main(String[] args) {
 		ArrayList list1 = new ArrayList(10);
@@ -69,3 +66,42 @@ list2:[0, 2, 4, AA, B, C]
 
 list1:[0, 2, 4]
 list2:[AA, B, C]
+
+
+//Ex11_2
+
+public class Ex11_2 {
+
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		Queue q = new LinkedList(); // Queue인터페이스의 구현체인 LinkedList를 사용
+
+		st.push("0");
+		st.push("1");
+		st.push("2");
+
+		q.offer("0");
+		q.offer("1");
+		q.offer("2");
+
+		System.out.println("= Stack =");
+		while (!st.empty()) {
+			System.out.println(st.pop()); // 스택에서 요소를 하나를 꺼내서 출력
+		}
+
+		System.out.println("= Queue =");
+		while (!q.isEmpty()) {
+			System.out.println(q.poll()); // 큐에서 요소를 하나를 꺼내서 출력
+		}
+	}
+}
+
+<Console>
+= Stack =
+2
+1
+0
+= Queue =
+0
+1
+2
