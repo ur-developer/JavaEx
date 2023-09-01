43057f480e406c73489c01346827c99b0f7f1707
Park Min Ki <ur_developer92@naver.com>
2023-09-01 22:14:45 +0900

    PC chapter11 add5

--- Ch.11.java
+++ Ch.11.java
@@ -228,3 +228,30 @@ history
 5.history
 >>
 q
+
+Ex11_5
+public class Ex11_5 {
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+			Object obj = it.next();
+			System.out.println(obj);
+		}
+	}
+}
+
+<Console>
+1
+2
+3
+4
+5
+
