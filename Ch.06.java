88bbf39c3f869252a15f57d708269b77525094f4
Park Min Ki <ur_developer92@naver.com>
2023-09-07 21:17:40 +0900

    PC chapter06 add3

--- Ch.06.java
+++ Ch.06.java
@@ -29,21 +29,28 @@ class Tv {
 현재 채널은 7 입니다.
 현재 채널은 6 입니다.
 
-public class TvTest2 {
-
+Ex6_2
+public class Ex6_2 {
 	public static void main(String[] args) {
-		Tv t1 = new Tv();
+		Tv t1 = new Tv();  // Tv t1; t1 = new Tv();를 한 문장으로 가능
 		Tv t2 = new Tv();
-		System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
-		System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
+		System.out.println("t1의 channel값은 "  +t1.channel + "입니다.");
+		System.out.println("t2의 channel값은 "  +t2.channel + "입니다.");
 
-		t1.channel = 7;
+		t1.channel = 7; // channel 값을 7로 한다.
 		System.out.println("t1의 channel값을 7로 변경하였습니다.");
 
-		System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
-		System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
+		System.out.println("t1의 channel값은 "  +t1.channel + "입니다.");
+		System.out.println("t2의 channel값은 "  +t2.channel + "입니다.");
 	}
 }
+
+<Console>
+t1의 channel값은 0입니다.
+t2의 channel값은 0입니다.
+t1의 channel값을 7로 변경하였습니다.
+t1의 channel값은 7입니다.
+t2의 channel값은 0입니다.
 
 public class TvTest3 {
 
