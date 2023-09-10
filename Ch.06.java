416697e3c29e422594d2a54e836da2a7aec4361f
Park Min Ki <ur_developer92@naver.com>
2023-09-10 23:00:26 +0900

    PC chapter06 add5

--- Ch.06.java
+++ Ch.06.java
@@ -110,44 +110,78 @@ public class MyMath {
 	}
 }
 
-
+//Ex6_4
 public class Ex6_4 {
-
 	public static void main(String[] args) {
-		MyMath mm = new MyMath();
-		long result1 = mm.add(5L, 3L);
-		long result2 = mm.subtract(5L, 3L);
-		long result3 = mm.multiply(5L, 3L);
-		double result4 = mm.divide(5L, 3L);
-		
-		System.out.println("add(5L, 3L) = " + result1);
-		System.out.println("subtract(5L, 3L) = " + result2);
-		System.out.println("multiply(5L, 3L) = " + result3);
-		System.out.println("divide(5L, 3L) = " + result4);
+	MyMath mm =  new MyMath();
+	long result1 = mm.add(5L, 3L);
+	long result2 = mm.subtract(5L, 3L);
+	long result3 = mm.multiply(5L, 3L);
+	double result4 = mm.divide(5L, 3L);
+	
+	System.out.println("add(5L, 3L) = " + result1);
+	System.out.println("usbtract(5L, 3L) = " + result2);
+	System.out.println("multiply(5L, 3L) = " + result3);
+	System.out.println("divide(5L, 3L) = " + result4);
 	}
 }
 
+class MyMath {
+	long add(long a, long b ) {
+		long result = a + b;
+		return result;
+		// return a + b; // 위의 두 줄을 이와 같이 한줄로 간단히 할 수 있다.
+		}
+		long subtract(long a, long b) {return a - b; }
+		long multiply(long a, long b) {return a * b; }
+		double divide(double a, double b) { 
+			return a / b;
+	}
+}
 
-public class Data { int x;}
+<Console>
+add(5L, 3L) = 8
+usbtract(5L, 3L) = 2
+multiply(5L, 3L) = 15
+divide(5L, 3L) = 1.6666666666666667
 
+//Ex6_5
+public class Ex6_5 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
+
+<Console>
+Hello
+
+
+//Ex6_6
+class Data { int x; }
 
 public class Ex6_6 {
 
 	public static void main(String[] args) {
-		Data d = new Data();
-		d.x = 10;
-		System.out.println("main() : x = " + d.x);
-		
-		change(d.x);
-		System.out.println("After change(d.x)");
-		System.out.println("main() : x = " + d.x);
-	}
-
-	static void change(int x) {
+			Data d = new Data();
+			d.x = 10;
+			System.out.println("main() : x = " + d.x);
+			
+			change(d.x);
+			System.out.println("After change(d.x)");
+			System.out.println("main() : x + " + d.x);
+		}
+	
+	static void change(int x) { //기본형 매개변수
 		x = 1000;
-		System.out.println("change() : x = " +x);
+		System.out.println("change() : x = " + x);
 	}
 }
+
+<Console>
+main() : x = 10
+change() : x = 1000
+After change(d.x)
+main() : x + 10
 
 public class Data2 { int x;}
 
