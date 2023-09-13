33b64fb4709a9d0e56020cd5f8295d56b4382ff5
Park Min Ki <ur_developer92@naver.com>
2023-09-13 23:55:22 +0900

    PC chapter06 add8

--- Ch.06.java
+++ Ch.06.java
@@ -283,6 +283,81 @@ public class Ex6_9 {
 20000
 2.0
 
+//Ex6_10
+public class Ex6_10 {
+	public static void main(String[] args) {
+		MyMath3 mm = new MyMath3();
+		System.out.println("mm.add(3, 3) 결과:"    + mm.add(3,3));
+		System.out.println("mm.add(3L, 3) 결과:"    + mm.add(3L,3));
+		System.out.println("mm.add(3, 3L) 결과:"    + mm.add(3,3L));
+		System.out.println("mm.add(3L, 3L) 결과:"    + mm.add(3L,3L));
+			
+		int[] a = {100, 200, 300};
+		System.out.println("mm.add(a) 결과: " + mm.add(a));
+	}
+}
+
+class MyMath3 { 
+	int add(int a, int b) {
+		System.out.println("int add(int a, int b) - ");
+		return a+b;
+	}
+
+	long add(int a, long b) {
+		System.out.println("long add(int a,  long b) - ");
+		return a+b;
+	}
+	
+	long add(long a, int b) {
+		System.out.println("long add(long a,  int b) - ");
+		return a+b;
+	}
+	
+	long add(long a, long b) {
+		System.out.println("long add(long a,  long b) - ");
+		return a+b;
+	}
+	
+	int add(int[] a) {
+		System.out.println("int add(int[] a) - ");
+		int result = 0;
+		for(int i=0; i < a.length; i++)	
+			result += a[i];
+		return result;
+	}
+}
+
+<Console>
+int add(int a, int b) - mm.add(3, 3) 결과:6
+long add(long a,  int b) - mm.add(3L, 3) 결과:6
+long add(int a,  long b) - mm.add(3, 3L) 결과:6
+long add(long a,  long b) - mm.add(3L, 3L) 결과:6
+int add(int[] a) - mm.add(a) 결과: 600
+
+//Ex_6-11
+class Data_1 {
+	int value;
+}
+
+class Data_2 {
+	int value;
+	
+	Data_2(int x) { // 매개변수가 있는 생성자
+		value = x;
+	}
+}
+
+public class Ex6_11 {
+	public static void main(String[] args) {
+		Data_1 data_1 = new Data_1();
+		Data_2 data_2 = new Data_2(); // compile error발생
+	}
+}
+
+<Console>
+Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+	The constructor Data_2() is undefined
+
 
 public class CallStackTest {
 
